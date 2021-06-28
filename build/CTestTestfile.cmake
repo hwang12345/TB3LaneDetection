7184f8b55dc4e31d72060c4b2a1f3ad926e543f2
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-06-28 13:19:08 +0200

    Added build directory.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/lorenz/Projects/ROS/TurtlebotFollower/build
+# Build directory: /home/lorenz/Projects/ROS/TurtlebotFollower/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
