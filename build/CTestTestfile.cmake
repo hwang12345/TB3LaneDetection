75646a3305f0c895c1d849e65370196cb7dfdb5c
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-06-28 13:23:29 +0200

    Cleaned build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lorenz/Projects/ROS/TurtlebotFollower/build
-# Build directory: /home/lorenz/Projects/ROS/TurtlebotFollower/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
