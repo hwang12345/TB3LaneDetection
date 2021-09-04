d0eaf25c68f512e96b08a8880258b7cef402e597
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-09-04 14:08:28 +0200

    Started implementing lane detector

--- include/lane_detection.h
+++ include/lane_detection.h
@@ -3,8 +3,16 @@
 
 #endif //ROSIMAGE_LANE_DETECTION_H
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+using namespace std;
+using namespace cv;
+
 class Lane {
 public:
-    sliding_window();
+    vector<Point2f> sliding_window();
 
 };
