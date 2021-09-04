3d5b0f158dce2dca40f327bdf3ab5f0234a0f9ca
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-09-04 15:01:13 +0200

    Cleaned up a little

--- include/img_proc.cpp
+++ include/img_proc.cpp
@@ -1,8 +1,6 @@
 #include <img_proc.h>
 
 Mat ImageProcessor::transform_img(Mat &img) {
-    //Prepare things that don't need to be computed on every frame.
-    Point2f srcVertices[4];
 
     // Coords for perspective transformation (counterclockwise as used by cv::warpPerspective)
 
@@ -11,6 +9,9 @@ Mat ImageProcessor::transform_img(Mat &img) {
     // cv::circle(cv_img, cv::Point(320, 180), 10, CV_RGB(0,255,0)); // Point 2 (Green)
     // cv::circle(cv_img, cv::Point(50, 180), 10, CV_RGB(0,0,255)); // Point 3 (Blue)
 
+    // Initialize vector for edges and define those
+    Point2f srcVertices[4];
+
     srcVertices[0] = Point(80, 130);
     srcVertices[1] = Point(280, 130);
     srcVertices[2] = Point(320, 180);
@@ -18,12 +19,13 @@ Mat ImageProcessor::transform_img(Mat &img) {
 
     //Destination vertices. Output is 640 by 480px
     Point2f dstVertices[4];
+
     dstVertices[0] = Point(0, 0);
     dstVertices[1] = Point(640, 0);
     dstVertices[2] = Point(640, 480);
     dstVertices[3] = Point(0, 480);
 
-    //Prepare matrix for transform and get the warped image
+    // Compute transform matrix and perform homographic transformation
     Mat perspectiveMatrix = getPerspectiveTransform(srcVertices, dstVertices);
     Mat dst(480, 640, CV_8UC3); //Destination for warped image
 
@@ -36,34 +38,34 @@ Mat ImageProcessor::mask_img(Mat &img) {
 
     cvtColor(img, img, COLOR_RGB2GRAY);
 
-    //Extract yellow and white info
+    //Mask yellow and white pixels of image
     Mat maskYellow, maskWhite;
 
     inRange(img, Scalar(20, 100, 100), Scalar(30, 255, 255), maskYellow);
     inRange(img, Scalar(150, 150, 150), Scalar(255, 255, 255), maskWhite);
 
+    // Combine yellow and white mask and replace in original
     Mat mask, processed;
-    bitwise_or(maskYellow, maskWhite, mask); //Combine the two masks
-    bitwise_and(img, mask, processed); //Extract what matches
+    bitwise_or(maskYellow, maskWhite, mask);
+    bitwise_and(img, mask, processed);
 
     return processed;
 }
 
 Mat ImageProcessor::blur(Mat &img) {
-    //Blur the image a bit so that gaps are smoother
+    //Blur to smooth irregularities
     const Size kernelSize = Size(9, 9);
     GaussianBlur(img, img, kernelSize, 0);
 
-    //Try to fill the gaps
+    //Fill gaps
     Mat kernel = Mat::ones(15, 15, CV_8U);
     dilate(img, img, kernel);
     erode(img, img, kernel);
     morphologyEx(img, img, MORPH_CLOSE, kernel);
 
-    //Keep only what's above 150 value, other is then black
+    //Threshold image
     const int thresholdVal = 150;
     threshold(img, img, thresholdVal, 255, THRESH_BINARY);
-    //Might be optimized with adaptive thresh
 
     return img;
 }
